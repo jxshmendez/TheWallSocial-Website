0c885f24386e537fe89d498911637f55254e7491
Josh Mendez <6joshmendez@gmail.com>
2025-12-12 20:07:49 +0000

    removed advent of code files, how did they get there

--- AdventOfCode1/src/main/java/TransactionProcessor.java
+++
@@ -1,7 +0,0 @@
-public class TransactionProcessor {
-
-    public static void transfer(BankAccount bank, BankAccount bankToTransfer, double amount){
-        double transferAmount = bank.withdraw(amount);
-        bankToTransfer.deposit(transferAmount);
-    }
-}

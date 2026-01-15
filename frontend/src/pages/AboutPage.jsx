fd690f716404251f549e382cf812609c1e471a64
Josh Mendez <6joshmendez@gmail.com>
2026-01-15 14:57:04 +0000

    fixing about section for mobile

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,10 +1,18 @@
 export default function AboutPage(){
     return (
-        <div className="w-full bg-black">
+        <div className="w-screen bg-black min-h-screen overflow-x-hidden" style={{ margin: 0, padding: 0 }}>
             <img 
                 src="/About.png" 
                 alt="About WALL"
                 className="w-full h-auto block"
+                style={{
+                    width: '100vw',
+                    maxWidth: '100%',
+                    height: 'auto',
+                    display: 'block',
+                    margin: 0,
+                    padding: 0
+                }}
             />
         </div>
     );
